25592354ce79c8e70f3ab0c66468b3eede319982
jrnold <jeffrey.arnold@gmail.com>
2018-08-10 08:30:31 -0700

    Add args to render script
    Allow user to specify which output to produce.

--- bin/render.R
+++ bin/render.R
@@ -1,5 +1,12 @@
 #!/usr/bin/env Rscript
+# Script to render book
 if (rmarkdown::pandoc_version() < 2) {
   stop("This book requires pandoc > 2")
 }
-bookdown::render_book(".", output_format = "all")
+args <- commandArgs(TRUE)
+output_format <- if (!length(args)) {
+  "all"
+} else {
+  args[[1]]
+}
+bookdown::render_book(".", output_format = output_format)
