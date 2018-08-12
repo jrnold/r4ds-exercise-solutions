9c756929ce610507d10fcbfe841e1331e39c65ee
jrnold <jeffrey.arnold@gmail.com>
2018-08-12 15:58:26 -0700

    render script checks that everything is committed

--- bin/render.R
+++ bin/render.R
@@ -1,12 +1,40 @@
 #!/usr/bin/env Rscript
 # Script to render book
-if (rmarkdown::pandoc_version() < 2) {
-  stop("This book requires pandoc > 2")
+
+# From devtools:::git_uncommitted
+git_uncommitted <- function (path = ".") {
+  r <- git2r::repository(path, discover = TRUE)
+  st <- vapply(git2r::status(r), length, integer(1))
+  any(st != 0)
 }
-args <- commandArgs(TRUE)
-output_format <- if (!length(args)) {
-  "all"
-} else {
-  args[[1]]
+
+# Adapted from devtools:::git_uncommitted
+check_uncommitted <- function(path = ".") {
+  if (!git_uncommitted(path)) {
+    stop("Uncommitted files.",
+         "All files should be committed before release.",
+         "Please add and commit.")
+  }
 }
-bookdown::render_book(".", output_format = output_format)
+
+render <- function(path = here::here(), output_format = "all") {
+  if (rmarkdown::pandoc_version() < 2) {
+    stop("This book requires pandoc > 2")
+  }
+  check_uncommitted(path)
+  bookdown::render_book(path, output_format = output_format)
+}
+
+main <- function(args = NULL) {
+  if (is.null(args)) {
+    args <- commandArgs(TRUE)
+  }
+  output_format <- if (!length(args)) {
+    "all"
+  } else {
+    args[[1]]
+  }
+  render(output_format = output_format)
+}
+
+main()
