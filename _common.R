3f94152c0438ff9938be5ba1ee438d04d89a501e
jrnold <jeffrey.arnold@gmail.com>
2018-08-17 10:02:25 -0700

    Update intro

--- _common.R
+++ _common.R
@@ -20,8 +20,7 @@ knitr::opts_chunk$set(
   fig.align = 'center',
   fig.width = 6,
   fig.asp = 0.618,  # 1 / phi
-  fig.show = "hold",
-  tidy = "styler"
+  fig.show = "hold"
 )
 
 options(dplyr.print_min = 6, dplyr.print_max = 6)
