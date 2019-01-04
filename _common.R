30a48c3eafa98fdc22acb0e628b41351be1d1db1
jrnold <jeffrey.arnold@gmail.com>
2019-01-03 16:12:03 -0800

    add tidy styler to R

--- _common.R
+++ _common.R
@@ -12,7 +12,9 @@ knitr::opts_chunk$set(
   fig.align = "center",
   fig.width = 6,
   fig.asp = 0.618, # 1 / phi
-  fig.show = "hold"
+  fig.show = "hold",
+  # styler
+  tidy = 'styler'
 )
 
 options(dplyr.print_min = 6, dplyr.print_max = 6)
