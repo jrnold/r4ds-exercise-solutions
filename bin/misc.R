1fbfc375d7786234fb51b13b3fac20d3acb045f9
jrnold <jeffrey.arnold@gmail.com>
2019-01-15 19:48:19 -0800

    add is_foreign_key script

--- bin/misc.R
+++ bin/misc.R
@@ -2,11 +2,21 @@ any_missing <- function(x) any(is.na(x))
 
 is_key <- function(tbl, ...) {
   variables <- quos(...)
-  has_nulls <- summarise_at(tbl, vars(!!!variables), any_missing)
+  has_nulls <- summarise_at(tbl, vars(UQS(variables)), any_missing)
   if (any(as.logical(has_nulls))) {
     return(FALSE)
   }
   nrow(distinct(tbl, !!!variables)) == nrow(tbl)
+}
+
+# check that columns in y are are foreign key of x
+is_foreign_key <- function(x, y, by = NULL) {
+  # check that y is a primary key
+  if (!rlang::eval_tidy(quo(is_key(y, !!!by)))) {
+    return(FALSE)
+  }
+  # check that all x are found in y
+  !nrow(anti_join(x, y, by = by))
 }
 
 foo <- tribble(
