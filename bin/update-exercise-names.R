295753ee35cb59647531abd53b6675532fb07f9e
jrnold <jeffrey.arnold@gmail.com>
2019-01-16 21:25:15 -0600

    script to update exercise names

--- bin/update-exercise-names.R
+++ bin/update-exercise-names.R
@@ -1,13 +1,11 @@
 library("readr")
 library("stringr")
-
-rmd_files <-
+library("purrr")
 
 clean_exercise <- function(path) {
   lines <- read_lines(path)
-  PAT <- "^(#+\\s*Exercise)\\s+(<span .*>)(.*)(</span>)\\s*(\\{.*)(\\})\\s*$"
-  newlines <- str_replace(lines, PAT,  "\\1 \\3 \\5 data-number=\"\\3\"}")
-  write_lines(newlines, path)
+  lines <- str_replace(lines, "^(#.*\\{#.*)\\}\\s*$", "\\1 .r4ds-section}")
+  write_lines(lines, path)
 }
 fs::dir_ls(path = ".", regexp = ".*\\.[rR]md$") %>%
   walk(clean_exercise)
